10dec7616e8f0959bc2676bcce2bbccba9dd2912
曾桂林 <zengguilin@pywm.com.cn>
2017-11-16 23:00:44 +0800

    第三章：第5节子路由

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,19 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ProductComponent } from './product/product.component';
 import { Code404Component } from './code404/code404.component';
+import {ProductDescComponent} from './product-desc/product-desc.component';
+import {SellerInfoComponent} from './seller-info/seller-info.component';
 
 // 在设置路由中，具体的路由放在最前面，通配符的路由放在最后面
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'product/:id/:name', component: ProductComponent },
+  { path: 'product/:id/:name', component: ProductComponent,
+    children: [
+      { path: '', component: ProductDescComponent },
+      { path: 'seller/:id', component: SellerInfoComponent }
+    ]
+  },
   { path: '**', component: Code404Component },
 ];
 
--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import {ActivatedRoute, Params, Router} from '@angular/router';
 
 @Component({
   selector: 'app-product',
@@ -10,14 +10,23 @@ export class ProductComponent implements OnInit {
 
   private productId: number;
   private productName: string;
-  constructor(private router: ActivatedRoute) { }
+  constructor(private routerInfo: ActivatedRoute,
+              private router: Router
+  ) { }
 
   ngOnInit() {
-    this.router.params.subscribe((params: Params) => {   // 路由参数订阅，建议所有的页面都使用参数订阅的方式取参数
+    this.routerInfo.params.subscribe((params: Params) => {   // 路由参数订阅，建议所有的页面都使用参数订阅的方式取参数
       this.productId = params['id'];
       this.productName = params['name'];
     });
     // this.productId = this.router.snapshot.params['id'];  // 路由参数快照，不同属性页面之间显示的时候可以使用
     // this.productName = this.router.snapshot.params['name'];
+  }
+  // TODO
+  toProductDesc () {
+    this.router.navigate(['./']);
+  }
+  toSeller () {
+    this.router.navigate(['./seller', 98]);
   }
 }
