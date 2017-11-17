8f09af3759a6fc7fbe16c43a5f19d43e35a4f250
MLKG <1059158548@qq.com>
2017-11-17 11:09:52 +0800

    第三章：第7节内容路由守卫

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,9 @@ import { ProductComponent } from './product/product.component';
 import { Code404Component } from './code404/code404.component';
 import { ProductDescComponent } from './product-desc/product-desc.component';
 import { SellerInfoComponent } from './seller-info/seller-info.component';
-import {ChatComponent} from './chat/chat.component';
+import { ChatComponent } from './chat/chat.component';
+import { LoginGuard } from './guard/login.guard';
+import { UnsaveGuard } from './guard/unsave.guard';
 
 // 在设置路由中，具体的路由放在最前面，通配符的路由放在最后面
 const routes: Routes = [
@@ -16,13 +18,19 @@ const routes: Routes = [
     children: [
       { path: '', component: ProductDescComponent },
       { path: 'seller/:id', component: SellerInfoComponent }
-    ]
+    ],
+    canActivate: [LoginGuard],
+    canDeactivate: [UnsaveGuard]
   },
   { path: '**', component: Code404Component },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [
+    LoginGuard,
+    UnsaveGuard
+  ]
 })
 export class AppRoutingModule { }
