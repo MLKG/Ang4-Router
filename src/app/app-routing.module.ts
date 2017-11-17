24cdc0d47521357d3ee6e5e928349721a6165f61
MLKG <1059158548@qq.com>
2017-11-17 10:23:14 +0800

    第三章：第6节内容辅助路由

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,13 @@ import { ProductComponent } from './product/product.component';
 import { Code404Component } from './code404/code404.component';
 import { ProductDescComponent } from './product-desc/product-desc.component';
 import { SellerInfoComponent } from './seller-info/seller-info.component';
+import {ChatComponent} from './chat/chat.component';
 
 // 在设置路由中，具体的路由放在最前面，通配符的路由放在最后面
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
+  { path: 'chat', component: ChatComponent, outlet: 'aux'},
   { path: 'product/:id/:name', component: ProductComponent,
     children: [
       { path: '', component: ProductDescComponent },
